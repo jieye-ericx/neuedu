f5b9d23185e5b27653d218b2e290f34c71213c85
radoapx <radoapx@gmail.com>
2020-05-08 23:40:31 +0800

    20200508 学习py

--- neuedu-py/2.数据分析/2.2-Numpy/01-初识numpy.py
+++ neuedu-py/2.数据分析/2.2-Numpy/01-初识numpy.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 # 1. 创建数组
 # 1.1 把一个list转换成ndarray
 # n1 = np.array([1, 2, 3])
@@ -21,8 +22,8 @@ import numpy as np
 # 创建n维的ndarray
 # 2. 创建n维的数组
 # 2.1 利用array()创建 可以传入ndmin这个缺省参数来指定ndarray的维度
-n4 = np.array([1, 2, 3,[4]], ndmin=2)
-print(n4)
+# n4 = np.array([1, 2, 3], ndmin=2)
+# print(n4)
 
 # 查看ndarray的维度(秩)
 # 返回的长度就是ndarray的维度
@@ -36,7 +37,7 @@ print(n4)
 # n4.shape = (1, 3)
 # print(n4)
 
-# ----------- demo
+# ------------- 例1
 # n5 = np.array([1, 2, 3, 4, 5, 6])
 # print(n5)
 # print(n5.ndim)
@@ -44,10 +45,30 @@ print(n4)
 # # 更改n5的形状
 # n5.shape = (2, 3)
 # print(n5)
+# print(n5.ndim)
+
 # ndarray的秩取决于形状（返回的元组的长度）
 # print(n5.ndim)
 
 
+# ------------- 例2
+n6 = np.arange(1, 13)
+# print(n6)
+# print(n6.ndim)
 
+# 更改维度为2
+# n6.shape = (4, 3)
+# print(n6)
+# print(n6.ndim)
+
+# 更改维度为3
+n6.shape = (3, 2, 2)
+print(n6)
+print(n6.ndim)
+
+# 注意reshape同样可以更改一个ndarray的维度,但是需要注意的是reshape不属于原地操作
+# 该方法是返回一个新的ndarray
+# n7 = n6.reshape(3, 4)
+# print(n7)
 
 
